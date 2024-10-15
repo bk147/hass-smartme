d90d18a4b9677acf6c05ba938df60b78adcbb194
timniklas <accounts@tnkoch.de>
2024-10-15 17:17:28 +0200

    Update hub.py

--- custom_components/smart-me/hub.py
+++ custom_components/smart-me/hub.py
@@ -13,118 +13,21 @@ from homeassistant.core import HomeAssistant
 
 
 class Hub:
-    """Dummy hub for Hello World example."""
-
-    manufacturer = "Demonstration Corp"
+    manufacturer = "Smartme Hub"
 
     def __init__(self, hass: HomeAssistant, deviceid: str, username: str, password: str) -> None:
         """Init dummy hub."""
-        self._host = deviceid
         self._hass = hass
-        self._name = deviceid
-        self._id = deviceid.lower()
-        self.rollers = [
-            Roller(f"{self._id}_1", f"{self._name} 1", self),
-            Roller(f"{self._id}_2", f"{self._name} 2", self),
-            Roller(f"{self._id}_3", f"{self._name} 3", self),
-        ]
+        self._deviceid = deviceid
+        self._username = username
+        self._password= password
         self.online = True
 
     @property
     def hub_id(self) -> str:
-        """ID for dummy hub."""
-        return self._id
+        """ID for hub."""
+        return self._deviceid
 
     async def test_connection(self) -> bool:
-        """Test connectivity to the Dummy hub is OK."""
-        await asyncio.sleep(1)
+        """TODO"""
         return True
-
-
-class Roller:
-    """Dummy roller (device for HA) for Hello World example."""
-
-    def __init__(self, rollerid: str, name: str, hub: Hub) -> None:
-        """Init dummy roller."""
-        self._id = rollerid
-        self.hub = hub
-        self.name = name
-        self._callbacks = set()
-        self._loop = asyncio.get_event_loop()
-        self._target_position = 100
-        self._current_position = 100
-        # Reports if the roller is moving up or down.
-        # >0 is up, <0 is down. This very much just for demonstration.
-        self.moving = 0
-
-        # Some static information about this device
-        self.firmware_version = f"0.0.{random.randint(1, 9)}"
-        self.model = "Test Device"
-
-    @property
-    def roller_id(self) -> str:
-        """Return ID for roller."""
-        return self._id
-
-    @property
-    def position(self):
-        """Return position for roller."""
-        return self._current_position
-
-    async def set_position(self, position: int) -> None:
-        """
-        Set dummy cover to the given position.
-
-        State is announced a random number of seconds later.
-        """
-        self._target_position = position
-
-        # Update the moving status, and broadcast the update
-        self.moving = position - 50
-        await self.publish_updates()
-
-        self._loop.create_task(self.delayed_update())
-
-    async def delayed_update(self) -> None:
-        """Publish updates, with a random delay to emulate interaction with device."""
-        await asyncio.sleep(random.randint(1, 10))
-        self.moving = 0
-        await self.publish_updates()
-
-    def register_callback(self, callback: Callable[[], None]) -> None:
-        """Register callback, called when Roller changes state."""
-        self._callbacks.add(callback)
-
-    def remove_callback(self, callback: Callable[[], None]) -> None:
-        """Remove previously registered callback."""
-        self._callbacks.discard(callback)
-
-    # In a real implementation, this library would call it's call backs when it was
-    # notified of any state changeds for the relevant device.
-    async def publish_updates(self) -> None:
-        """Schedule call all registered callbacks."""
-        self._current_position = self._target_position
-        for callback in self._callbacks:
-            callback()
-
-    @property
-    def online(self) -> float:
-        """Roller is online."""
-        # The dummy roller is offline about 10% of the time. Returns True if online,
-        # False if offline.
-        return random.random() > 0.1
-
-    @property
-    def battery_level(self) -> int:
-        """Battery level as a percentage."""
-        return random.randint(0, 100)
-
-    @property
-    def battery_voltage(self) -> float:
-        """Return a random voltage roughly that of a 12v battery."""
-        return round(random.random() * 3 + 10, 2)
-
-    @property
-    def illuminance(self) -> int:
-        """Return a sample illuminance in lux."""
-        return random.randint(0, 500)
