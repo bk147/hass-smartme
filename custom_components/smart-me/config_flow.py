6aef449da474978251845cb848ed0dc1c3871ed0
timniklas <accounts@tnkoch.de>
2024-10-15 13:52:57 +0200

    Update config_flow.py

--- custom_components/smart-me/config_flow.py
+++ custom_components/smart-me/config_flow.py
@@ -10,21 +10,22 @@ class SmartmeConfigFlow(ConfigFlow, domain=DOMAIN):
 
     def __init__(self) -> None:
         """Initialize the config flow."""
+        self._session = async_get_clientsession(self.hass)
         self._username: None = None
         self._password: None = None
         self._deviceid:  None = None
         self._discovered_devices: dict[str, str] = {}
   
-    async def async_step_user(self, userdata):
+    async def async_step_user(self, formdata):
         if userdata is not None:
-            websession = async_get_clientsession(self.hass)
-            self._username = userdata['username']
-            self._password = userdata['password']
+            self._username = formdata['username']
+            self._password = formdata['password']
             try:
-                async with websession.get(url="https://api.smart-me.com/Devices", auth=BasicAuth(self._username, self._password)) as response:
+                async with self._session.get(url="https://api.smart-me.com/Devices", auth=BasicAuth(self._username, self._password)) as response:
                     response.raise_for_status()
-                    response_data = await response.json() 
-                    print(response_data)
+                    response_data = await response.json()
+                    for device in response_data:
+                         self._discovered_devices[device.Id] = device.Name
             except ClientResponseError as exc:
                 return self.async_abort(reason="authentication")
             except ClientError as exc:
@@ -35,4 +36,15 @@ class SmartmeConfigFlow(ConfigFlow, domain=DOMAIN):
               vol.Required("username"): str,
               vol.Required("password"): str
             })
+        )
+  
+    async def async_step_device(self, formdata):
+        if formdata is not None:
+            return self.async_abort(reason="unknown")
+
+        return self.async_show_form(
+            step_id="device",
+            data_schema=vol.Schema(
+                {vol.Required("deviceid"): vol.In(self._discovered_devices)}
+            ),
         )
