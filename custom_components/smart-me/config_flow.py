043e94f9e272554decb950133a6b41f16be7b34b
timniklas <accounts@tnkoch.de>
2024-10-15 13:35:30 +0200

    Update config_flow.py

--- custom_components/smart-me/config_flow.py
+++ custom_components/smart-me/config_flow.py
@@ -1,31 +1,30 @@
-from __future__ import annotations
-
-from typing import Any
-
 import voluptuous as vol
-
-from homeassistant.components.bluetooth import (
-    BluetoothServiceInfoBleak,
-    async_discovered_service_info,
-)
 from homeassistant.config_entries import ConfigFlow
-from homeassistant.const import CONF_ADDRESS
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from aiohttp import ClientError, ClientResponseError, ClientSession, BasicAuth
 
 from .const import DOMAIN
 
 class SmartmeConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
   
-    async def async_step_user(self, info):
-        if info is not None:
-            return self.async_abort(reason="connenction")
-            pass  # TODO: process info
+    async def async_step_user(self, userdata):
+        if userdata is not None:
+            websession = async_get_clientsession(self.hass)
+            username = userdata['username']
+            password = userdata['password']
+            try:
+                async with websession.get(url=url, auth=BasicAuth(username, password)) as response:
+                    response.raise_for_status()
+                    print(response.json())
+            except ClientResponseError as exc:
+                return self.async_abort(reason="authentication")
+            except ClientError as exc:
+                return self.async_abort(reason="connenction")
 
         return self.async_show_form(
             step_id="user", data_schema=vol.Schema({
               vol.Required("username"): str,
-              vol.Required("password"): str,
-              vol.Required("deviceid"): str
+              vol.Required("password"): str
             })
         )
