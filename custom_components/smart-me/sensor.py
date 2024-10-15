956317275526d2efbc1268a9168707bf7e658da9
timniklas <accounts@tnkoch.de>
2024-10-15 17:52:19 +0200

    Update sensor.py

--- custom_components/smart-me/sensor.py
+++ custom_components/smart-me/sensor.py
@@ -22,7 +22,7 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     hub = hass.data[DOMAIN][config_entry.entry_id]
-    async_add_entities([GoveeBluetoothLight(hub)])
+    async_add_entities([ExampleSensor(hub)])
 
 class ExampleSensor(SensorEntity):
     """Representation of a Sensor."""
