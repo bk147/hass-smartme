4ddcfe3ad3dc36523d6af600e0d2a98fd89cd984
timniklas <accounts@tnkoch.de>
2024-10-15 12:57:41 +0200

    Update config_flow.py

--- custom_components/smart-me/config_flow.py
+++ custom_components/smart-me/config_flow.py
@@ -1,1 +1,31 @@
-##TODO
+from __future__ import annotations
+
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.components.bluetooth import (
+    BluetoothServiceInfoBleak,
+    async_discovered_service_info,
+)
+from homeassistant.config_entries import ConfigFlow
+from homeassistant.const import CONF_ADDRESS
+from homeassistant.data_entry_flow import FlowResult
+
+from .const import DOMAIN
+
+
+class SmartmeConfigFlow(ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+  
+    async def async_step_user(self, info):
+        if info is not None:
+            pass  # TODO: process info
+
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({
+              vol.Required("username"): str,
+              vol.Required("password"): str,
+              vol.Required("deviceid"): str
+            })
+        )
