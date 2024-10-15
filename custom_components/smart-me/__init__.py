7e86a62ede0c717f6a215a0cf6a7d20bd05837ec
timniklas <accounts@tnkoch.de>
2024-10-15 17:28:55 +0200

    Update __init__.py

--- custom_components/smart-me/__init__.py
+++ custom_components/smart-me/__init__.py
@@ -6,23 +6,19 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
 
-from . import hub
-
 # List of platforms to support. There should be a matching .py file for each,
 # eg <cover.py> and <sensor.py>
 PLATFORMS = [Platform.SENSOR]
 
-type HubConfigEntry = ConfigEntry[hub.Hub]
+class Hub:
+    def __init__(self, deviceid: str, username: str, password: str) -> None:
+        self.deviceid = deviceid
+        self.username = username
+        self.password= password
 
-
-async def async_setup_entry(hass: HomeAssistant, entry: HubConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Hello World from a config entry."""
-    # Store an instance of the "connecting" class that does the work of speaking
-    # with your actual devices.
-    entry.runtime_data = hub.Hub(hass, entry.unique_id, entry.data["username"], entry.data["password"])
-    
-    # This creates each HA object for each platform your device requires.
-    # It's done by calling the `async_setup_entry` function in each platform module.
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = Hub(hass, entry.unique_id, entry.data["username"], entry.data["password"])
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
