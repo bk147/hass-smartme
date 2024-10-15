4a45709f4209898d56e052029ccca6954d5487a6
timniklas <accounts@tnkoch.de>
2024-10-15 13:43:43 +0200

    Update config_flow.py

--- custom_components/smart-me/config_flow.py
+++ custom_components/smart-me/config_flow.py
@@ -7,16 +7,24 @@ from .const import DOMAIN
 
 class SmartmeConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
+
+    def __init__(self) -> None:
+        """Initialize the config flow."""
+        self._username: None = None
+        self._password: None = None
+        self._deviceid:  None = None
+        self._discovered_devices: [] = {}
   
     async def async_step_user(self, userdata):
         if userdata is not None:
             websession = async_get_clientsession(self.hass)
-            username = userdata['username']
-            password = userdata['password']
+            self._username = userdata['username']
+            self._password = userdata['password']
             try:
-                async with websession.get(url="https://api.smart-me.com/Devices", auth=BasicAuth(username, password)) as response:
+                async with websession.get(url="https://api.smart-me.com/Devices", auth=BasicAuth(self._username, self._password)) as response:
                     response.raise_for_status()
-                    print(response.json())
+                    response_data = await response.json() 
+                    print(response_data)
             except ClientResponseError as exc:
                 return self.async_abort(reason="authentication")
             except ClientError as exc:
