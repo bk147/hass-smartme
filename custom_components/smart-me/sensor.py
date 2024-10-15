9f2d21f2ecf1a54da8d3de5458ee54c57c4c9478
timniklas <accounts@tnkoch.de>
2024-10-15 18:44:34 +0200

    Update sensor.py

--- custom_components/smart-me/sensor.py
+++ custom_components/smart-me/sensor.py
@@ -5,77 +5,81 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
-)
-from homeassistant.const import UnitOfTemperature
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-
-from . import HubConfigEntry
 from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     hub = hass.data[DOMAIN][config_entry.entry_id]
-    async_add_entities([ExampleSensor(hub)])
+    async_add_entities([BatterySensor(hub)])
 
-class ExampleSensor(SensorEntity):
-    """Representation of a Sensor."""
+class SensorBase(Entity):
+    """Base representation of a Sensor."""
 
     should_poll = False
-    
-    _attr_name = "Example Temperature"
-    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
-    _attr_state_class = SensorStateClass.MEASUREMENT
 
-    def __init__(self, hub) -> None:
-        """Initialize a sensor."""
+    def __init__(self, hub):
+        """Initialize the sensor."""
         self._deviceid = hub.deviceid
-        self._devicename = hub.devicename
         self._username = hub.username
         self._password = hub.password
-
         self._callbacks = set()
-        
-        self._attr_unique_id = f"{self._deviceid}_device1"
-        self._attr_name = f"Device {self._deviceid}"
-        self._state = 25
 
+    # To link this entity to the cover device, this property must return an
+    # identifiers value matching that used in the device, but no other information such
+    # as name. If name is returned, this entity will then also become a device in the
+    # HA UI.
     @property
     def device_info(self):
         """Return information to link this entity with the correct device."""
         return {"identifiers": {(DOMAIN, self._deviceid)}}
-        
-    # The value of this sensor.
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-    
+
     # This property is important to let HA know if this entity is online or not.
     # If an entity is offline (return False), the UI will refelect this.
     @property
     def available(self) -> bool:
-        """Return True if device is available."""
+        """Return True if hub is available."""
         return True
 
     async def async_added_to_hass(self):
         """Run when this Entity has been added to HA."""
         # Sensors should also register callbacks to HA when their state changes
-        self._callbacks.add(callback)
+        self._callbacks.add(self.async_write_ha_state)
 
     async def async_will_remove_from_hass(self):
         """Entity being removed from hass."""
         # The opposite of async_added_to_hass. Remove any registered call backs here.
-        self._callbacks.discard(callback)
+        self._callbacks.discard(self.async_write_ha_state)
 
-    async def _updateState(self, state) -> None:
-        """Schedule call all registered callbacks."""
-        self._state = state
-        for callback in self._callbacks:
-            callback()
 
+class BatterySensor(SensorBase):
+    """Representation of a Sensor."""
+
+    # The class of this device. Note the value should come from the homeassistant.const
+    # module. More information on the available devices classes can be seen here:
+    # https://developers.home-assistant.io/docs/core/entity/sensor
+    device_class = SensorDeviceClass.BATTERY
+
+    # The unit of measurement for this entity. As it's a DEVICE_CLASS_BATTERY, this
+    # should be PERCENTAGE. A number of units are supported by HA, for some
+    # examples, see:
+    # https://developers.home-assistant.io/docs/core/entity/sensor#available-device-classes
+    _attr_unit_of_measurement = PERCENTAGE
+
+    def __init__(self, base):
+        """Initialize the sensor."""
+        super().__init__(base)
+
+        # As per the sensor, this must be a unique value within this domain. This is done
+        # by using the device ID, and appending "_battery"
+        self._attr_unique_id = f"{self.deviceid}_battery"
+
+        # The name of the entity
+        self._attr_name = f"Device {self.deviceid}"
+
+        self._state = 25
+
+    # The value of this sensor. As this is a DEVICE_CLASS_BATTERY, this value must be
+    # the battery level as a percentage (between 0 and 100)
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
