86491f1df2a3378f9a2791f6f4ded099425d2409
timniklas <accounts@tnkoch.de>
2024-10-15 17:47:32 +0200

    Create hub.py

---
+++ custom_components/smart-me/hub.py
@@ -0,0 +1,16 @@
+class Hub:
+    manufacturer = "Smartme"
+    
+    def __init__(self, deviceid: str, username: str, password: str) -> None:
+        self.deviceid = deviceid
+        self.username = username
+        self.password= password
+
+    @property
+    def hub_id(self) -> str:
+        """ID for hub."""
+        return self.deviceid
+
+    async def test_connection(self) -> bool:
+        """Test connectivity to the Dummy hub is OK."""
+        return True
