13dde37cdb63feb1bca9a5641eb1f148c02920bf
timniklas <accounts@tnkoch.de>
2024-10-15 18:21:03 +0200

    Delete custom_components/smart-me/hub.py

--- custom_components/smart-me/hub.py
+++
@@ -1,5 +0,0 @@
-class Hub:
-    def __init__(self, deviceid: str, username: str, password: str) -> None:
-        self.deviceid = deviceid
-        self.username = username
-        self.password = password
