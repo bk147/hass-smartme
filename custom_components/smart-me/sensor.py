320d9d9dc4ff47fd2a2ca104aec43c7f4442cd35
timniklas <accounts@tnkoch.de>
2024-10-16 11:01:59 +0200

    Update sensor.py

--- custom_components/smart-me/sensor.py
+++ custom_components/smart-me/sensor.py
@@ -36,14 +36,14 @@ async def async_setup_entry(
     # to a list for each one.
     # This maybe different in your specific case, depending on how your data is structured
     sensors = [
-        SmartmeSensor(coordinator)
+        SmartmeDeviceSensor(coordinator),
+        SmartmeSensor1(coordinator),
     ]
 
     # Create the sensors.
     async_add_entities(sensors)
 
-
-class SmartmeSensor(CoordinatorEntity, SensorEntity):
+class SmartmeDeviceSensor(CoordinatorEntity):
     """Implementation of a sensor."""
 
     def __init__(self, coordinator: SmartmeCoordinator) -> None:
@@ -85,7 +85,7 @@ class SmartmeSensor(CoordinatorEntity, SensorEntity):
     @property
     def name(self) -> str:
         """Return the name of the sensor."""
-        return "Test Device"
+        return "Test Device Sensor"
 
     @property
     def native_value(self) -> int | float:
@@ -110,7 +110,80 @@ class SmartmeSensor(CoordinatorEntity, SensorEntity):
         """Return unique id."""
         # All entities must have a unique id.  Think carefully what you want this to be as
         # changing it later will cause HA to create new entities.
-        return f"{DOMAIN}-{self.device_id}"
+        return f"{DOMAIN}-{self.device_id}-a"
+
+    @property
+    def extra_state_attributes(self):
+        """Return the extra state attributes."""
+        # Add any additional attributes you want on your sensor.
+        attrs = {}
+        attrs["extra_info"] = "Extra Info"
+        return attrs
+
+class SmartmeSensor(CoordinatorEntity):
+    """Implementation of a sensor."""
+
+    def __init__(self, coordinator: SmartmeCoordinator) -> None:
+        """Initialise sensor."""
+        super().__init__(coordinator)
+        self.device_id = f"{coordinator.deviceid}-abc123"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Update sensor with latest data from coordinator."""
+        # This method is called by your DataUpdateCoordinator when a successful update runs.
+        self.async_write_ha_state()
+
+    @property
+    def device_class(self) -> str:
+        """Return device class."""
+        # https://developers.home-assistant.io/docs/core/entity/sensor/#available-device-classes
+        return SensorDeviceClass.TEMPERATURE
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device information."""
+        # Identifiers are what group entities into the same device.
+        # If your device is created elsewhere, you can just specify the indentifiers parameter.
+        # If your device connects via another device, add via_device parameter with the indentifiers of that device.
+        return DeviceInfo(
+            identifiers={
+                (
+                    DOMAIN,
+                    f"{self.coordinator.data.controller_name}-{self.device_id}",
+                )
+            },
+        )
+
+    @property
+    def name(self) -> str:
+        """Return the name of the sensor."""
+        return "Test Sensor"
+
+    @property
+    def native_value(self) -> int | float:
+        """Return the state of the entity."""
+        # Using native value and native unit of measurement, allows you to change units
+        # in Lovelace and HA will automatically calculate the correct value.
+        return 99
+
+    @property
+    def native_unit_of_measurement(self) -> str | None:
+        """Return unit of temperature."""
+        return UnitOfTemperature.CELSIUS
+
+    @property
+    def state_class(self) -> str | None:
+        """Return state class."""
+        # https://developers.home-assistant.io/docs/core/entity/sensor/#available-state-classes
+        return SensorStateClass.MEASUREMENT
+
+    @property
+    def unique_id(self) -> str:
+        """Return unique id."""
+        # All entities must have a unique id.  Think carefully what you want this to be as
+        # changing it later will cause HA to create new entities.
+        return f"{DOMAIN}-{self.device_id}-b"
 
     @property
     def extra_state_attributes(self):
