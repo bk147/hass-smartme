ad35dd919f8a9bc30b733b5da08c98233402d0e5
timniklas <accounts@tnkoch.de>
2024-10-16 12:10:56 +0200

    Update config_flow.py

--- custom_components/smart-me/config_flow.py
+++ custom_components/smart-me/config_flow.py
@@ -5,6 +5,7 @@ from aiohttp import ClientError, ClientResponseError, ClientSession, BasicAuth
 
 from homeassistant.const import (
     CONF_DEVICE_ID,
+    CONF_FRIENDLY_NAME,
     CONF_USERNAME,
     CONF_PASSWORD,
 )
@@ -53,6 +54,7 @@ class SmartmeConfigFlow(ConfigFlow, domain=DOMAIN):
             await self.async_set_unique_id(deviceid, raise_on_progress=False)
             self._abort_if_unique_id_configured()
             self._data[CONF_DEVICE_ID] = deviceid
+            self._data[CONF_FRIENDLY_NAME] = self._discovered_devices[deviceid]
             self._data[CONF_USERNAME] = self._username
             self._data[CONF_PASSWORD] = self._password
             return self.async_create_entry(title=self._discovered_devices[deviceid], data=self._data)
