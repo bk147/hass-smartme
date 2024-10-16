49604d9ec50fd2dea1880e9d56bb00c9bbf7889c
timniklas <accounts@tnkoch.de>
2024-10-16 10:44:29 +0200

    Update api.py

--- custom_components/smart-me/api.py
+++ custom_components/smart-me/api.py
@@ -14,18 +14,6 @@ class DeviceType(StrEnum):
     OTHER = "other"
 
 
-DEVICES = [
-    {"id": 1, "type": DeviceType.TEMP_SENSOR},
-    {"id": 2, "type": DeviceType.TEMP_SENSOR},
-    {"id": 3, "type": DeviceType.TEMP_SENSOR},
-    {"id": 4, "type": DeviceType.TEMP_SENSOR},
-    {"id": 1, "type": DeviceType.DOOR_SENSOR},
-    {"id": 2, "type": DeviceType.DOOR_SENSOR},
-    {"id": 3, "type": DeviceType.DOOR_SENSOR},
-    {"id": 4, "type": DeviceType.DOOR_SENSOR},
-]
-
-
 @dataclass
 class Device:
     """API device."""
@@ -63,45 +51,6 @@ class API:
         """Disconnect from api."""
         self.connected = False
         return True
-
-    def get_devices(self) -> list[Device]:
-        """Get devices on api."""
-        return [
-            Device(
-                device_id=device.get("id"),
-                device_unique_id=self.get_device_unique_id(
-                    device.get("id"), device.get("type")
-                ),
-                device_type=device.get("type"),
-                name=self.get_device_name(device.get("id"), device.get("type")),
-                state=self.get_device_value(device.get("id"), device.get("type")),
-            )
-            for device in DEVICES
-        ]
-
-    def get_device_unique_id(self, device_id: str, device_type: DeviceType) -> str:
-        """Return a unique device id."""
-        if device_type == DeviceType.DOOR_SENSOR:
-            return f"{self.controller_name}_D{device_id}"
-        if device_type == DeviceType.TEMP_SENSOR:
-            return f"{self.controller_name}_T{device_id}"
-        return f"{self.controller_name}_Z{device_id}"
-
-    def get_device_name(self, device_id: str, device_type: DeviceType) -> str:
-        """Return the device name."""
-        if device_type == DeviceType.DOOR_SENSOR:
-            return f"DoorSensor{device_id}"
-        if device_type == DeviceType.TEMP_SENSOR:
-            return f"TempSensor{device_id}"
-        return f"OtherSensor{device_id}"
-
-    def get_device_value(self, device_id: str, device_type: DeviceType) -> int | bool:
-        """Get device random value."""
-        if device_type == DeviceType.DOOR_SENSOR:
-            return choice([True, False])
-        if device_type == DeviceType.TEMP_SENSOR:
-            return randrange(15, 28)
-        return randrange(1, 10)
 
 
 class APIAuthError(Exception):
